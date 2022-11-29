04cce77db7f5b994912042ce1c9b24e3ab3ca395
guilherme01 <lopesgui01@gmail.com>
2022-11-29 15:35:00 -0300

    tentativa de juntar os graficos

--- regressao.py
+++ regressao.py
@@ -68,11 +68,17 @@ def Regressao(X , y , imprimirMetricas = False ,tituloGrafico = False,axis = ('x
 # %%
 def violinePlot(df,qtClasses,
 				tituloGrafico = False,
+				imprimirMetricas = False,
 				axis = ('x','y'),
 				xLabel = "densidade",
 				yLabel = "taxa_aprovacao"):
-	df = df[np.isnan(df[yLabel]) !=True ]  
-	df = df[np.isnan(df[xLabel]) !=True ]  
+	
+	df = df[(np.isnan(df[yLabel]) !=True) & (np.isnan(df[xLabel]) !=True) ]  
+	# df = df[ ]  
+	y = df[xLabel].to_numpy()
+	X = df[yLabel].to_numpy()
+	# y = df[xLabel].to_numpy()
+	# X = df[yLabel].to_numpy()
 	dataset = []
 	intervaloClasse = ((X.max()-X.min()) /qtClasses)
 	minClass = 0.0 
@@ -96,27 +102,54 @@ def violinePlot(df,qtClasses,
 
 	ax.yaxis.grid(True)
 	# ax.violinplot(dataset=dataset)
-	ax.violinplot(dataset, pos,  points=200, widths=4,
-						showmeans=True, showextrema=True, showmedians=True)
+	ax.violinplot(dataset, pos,  
+						points=200, 
+						widths=2,
+						showmeans=True, 
+						showextrema=True, 
+						showmedians=True)
 	plt.xlabel(axis[0])
 	plt.ylabel(axis[1])
+
+	# #regressao
+
+
+	# X = X.reshape(-1,1)
+	# y = y.reshape(-1,1)
+	# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+	# regr = LinearRegression().fit(X_train, y_train)
+
+	# # Realizar predição com os dados separados para teste
+	# y_pred = regr.predict(X_train)
+	# metricas = [
+	# 		['Mean squared error:',mean_squared_error(y_train, y_pred)],
+	# 		['R2 Score:' , r2_score(y_train, y_pred)],
+	# 		['MAE:',  mean_absolute_error(y_train, y_pred)],
+	# 		['score', regr.score(X,y)],
+	# 		['intercept_', regr.intercept_],
+	# 		['coef_' ,regr.coef_[0][0]],
+	# 		['funcao','y = '+str(regr.intercept_[0])+'+('+str(regr.coef_[0][0])+'x)']
+	# 	]
+	# ax.plot(X_train, y_pred   , color='blue', linewidth=3)
+		
+	# if imprimirMetricas == True:
+	# 	print(metricas)
 	if tituloGrafico != False:
 		plt.title(tituloGrafico) 
 		plt.show()
 		plt.savefig(tituloGrafico,transparent = True)
+	# return metricas
 # %% [markdown]
 # lendo dados do csv para um data frame
 # %%
 # df = pd.read_csv('/content/drive/MyDrive/db.csv')
 dfB = pd.read_csv('db2.csv')
-df = dfB[np.isnan(dfB["taxa_aprovacao"]) !=True ]
-df.isnull().any()
 # %%
 
 
 # %%
-y = df['taxa_aprovacao']
-X = df['densidade']
+# y = df['taxa_aprovacao']
+# X = df['densidade']
 # criando um violin plots
 # %%
 # separando dados em classes
@@ -125,40 +158,42 @@ X = df['densidade']
 
 
 # %% imprimindo distribuicao normal da taxa_aprovacao 
+# # %%
+
+# plt.hist(X, 300) 
+# plt.show() 
+
+# plt.hist(y, 300) 
+# plt.show()
+# # %%
+# X = Normalizer().fit([X]).transform([X])
+# y = Normalizer().fit([y]).transform([y])
+
+# # %% 
+# # Selecionando Variáveis para o modelo
+# metricasNormalizadas = Regressao(X,y,True,'taxa aprovação X densidade de conexao normalizado',('taxa aprovação','densidade de conexao'))
+# y = df[['taxa_aprovacao']].to_numpy()
+# X = df[['densidade']].to_numpy()
+# metricasNaoNormalizadas = Regressao(X,y,True,'taxa aprovação X densidade de conexao',('taxa aprovação','densidade de conexao'))
 # %%
-
-plt.hist(X, 300) 
-plt.show() 
-
-plt.hist(y, 300) 
-plt.show()
-# %%
-X = Normalizer().fit([X]).transform([X])
-y = Normalizer().fit([y]).transform([y])
-
-# %% 
-# Selecionando Variáveis para o modelo
-metricasNormalizadas = Regressao(X,y,True,'taxa aprovação X densidade de conexao normalizado',('taxa aprovação','densidade de conexao'))
-y = df[['taxa_aprovacao']].to_numpy()
-X = df[['densidade']].to_numpy()
-metricasNaoNormalizadas = Regressao(X,y,True,'taxa aprovação X densidade de conexao',('taxa aprovação','densidade de conexao'))
-violinePlot(df = dfB,
-			qtClasses=30,
+violinePlot(df = pd.read_csv('db2.csv'),
+			qtClasses=20,
 			tituloGrafico="taxa aprovação X densidade de conexao distribuicao",
 			axis=("densidade de conexao","taxa aprovação ")
 			)
+			# %%
 violinePlot(df = dfB,
-			qtClasses = 30,
+			qtClasses = 20,
 			tituloGrafico = "indicador rendimento X densidade de conexao distribuicao",
 			axis=("densidade de conexao","taxa aprovação "),
 			yLabel = "indicador_rendimento")
 violinePlot(df = dfB,
-			qtClasses = 30,
+			qtClasses = 20,
 			tituloGrafico = "indicador rendimento X densidade de conexao distribuicao",
 			axis=("densidade de conexao","nota saeb matematica"),
 			yLabel = "nota_saeb_matematica")
 violinePlot(df = dfB,
-			qtClasses = 30,
+			qtClasses = 20,
 			tituloGrafico = "nota saeb lingua portuguesa X densidade de conexao distribuicao",
 			axis=("densidade de conexao","nota saeb lingua portuguesa "),
 			yLabel = "nota_saeb_lingua_portuguesa")
